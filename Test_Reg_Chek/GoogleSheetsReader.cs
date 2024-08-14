df0f3fc74c2d71224978a30ded167c3bb69cfc9c
Alanator1099 <106380912+Alanator1099@users.noreply.github.com>
2024-08-14 18:34:56 +0300

    Добавьте файлы проекта.

---
+++ Test_Reg_Chek/GoogleSheetsReader.cs
@@ -0,0 +1,69 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Reg_Chek
+{
+    internal class GoogleSheetsReader
+    {
+        private readonly string _sheetId;
+        private readonly SheetsService _service;
+        public List<String> tags = new();
+
+        public GoogleSheetsReader(string credentialsPath, string sheetId) 
+        {
+            _sheetId = sheetId;
+
+            GoogleCredential credential;
+
+            using (var stream = new System.IO.FileStream(credentialsPath, System.IO.FileMode.Open))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(SheetsService.Scope.SpreadsheetsReadonly);
+            }
+
+            _service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential
+            });
+        }
+
+        public async Task GetListOfTages(string sheetName, string row) 
+        {
+            var range = $"{sheetName}!{row}:{row}";
+            var request = _service.Spreadsheets.Values.Get(_sheetId, range);
+
+            ValueRange response;
+
+            try
+            {
+                response = await request.ExecuteAsync();
+            }
+            catch (System.Exception ex)
+            { 
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            //Parsing
+            if (response != null && response.Values != null)
+            {
+                foreach(var cell in response.Values)
+                {
+                    
+                    if (cell.Count > 0) 
+                    {
+                        tags.Add(cell[0].ToString()); 
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ Test_Reg_Chek/Program.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using Google.Apis.Sheets.v4;
+using Microsoft.VisualBasic;
+
+namespace Test_Reg_Chek
+{
+    internal class Program
+    {
+        static List<string> tags;
+        static string[] token;
+        static void Main(string[] args)
+        {
+            
+            token = System.IO.File.ReadAllText("C:/configs/RegBotConfigs.txt").Split("\r\n");
+
+            //Google API
+            UpdateTagsList();
+            //API телеграмм
+            var client = new TelegramBotClient(token[0]);
+            client.StartReceiving(Update, Error);
+            Console.ReadLine();
+        }
+
+        async static Task UpdateTagsList() 
+        {
+            var regDock = new GoogleSheetsReader("C:/configs/regchekertelegrambot.json", token[1]);
+            await regDock.GetListOfTages("Ответы на форму", "E");
+            //Console.WriteLine(regDock.tags[0]);
+            tags = regDock.tags;
+        }
+
+       async static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            Console.WriteLine(exception);
+        }
+
+        async static Task Update(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if (message.Text != null)
+            {
+                switch (message.Text)
+                {
+                    case "/start":
+                        await client.SendTextMessageAsync(update.Message.Chat.Id, "Привет я помогу тебе узнать зарегистрирован ли ты на мероприятие N.\r\nПришли мне список телеграмм тегов участников и я проверю их регистрацию.\r\nПравильно  составленный запрос должен выглядеть так:");
+                        await client.SendTextMessageAsync(update.Message.Chat.Id, "@TelegramTeg\r\n@TelegramTeg\r\n@TelegramTeg");
+                        break;
+                    case "/help":
+                        await client.SendTextMessageAsync(update.Message.Chat.Id, "Этот Telegram-бот создан для упрощения процесса проверки регистрации участников соревнований. Основная цель бота — предоставить удобный инструмент, с помощью которого участники мероприятий могут быстро и легко проверить свои регистрации.\r\n\r\ngit:");
+                        break;
+                    default:
+                        ChekTages(message.Text, client, update);
+                        break;
+                }
+            }
+        }
+
+        async static void ChekTages(string message, ITelegramBotClient client, Update update) 
+        {
+            string[] rows = message.Split("\n");
+            foreach (string row in rows)
+            {
+                if (tags.Contains(row))
+                {
+                    await client.SendTextMessageAsync(update.Message.Chat.Id, $"Участник с тегом {row} зарегистрирован");
+                }
+                else
+                {
+                    await client.SendTextMessageAsync(update.Message.Chat.Id, $"Участник с тегом {row} не найден");
+                }
+            }
+        }
+    }
+    
+}
+
